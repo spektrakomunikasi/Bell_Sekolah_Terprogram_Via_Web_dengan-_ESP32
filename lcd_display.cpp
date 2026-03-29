3c2dad885f6433fdecd8ee2e1a39333fc1356064
SPEKTRA KOMUNIKASI <spektrakomunikasi@gmail.com>
2026-03-29 14:10:16 +0700

    Implement lcd_display.cpp for LCD menu system

---
+++ lcd_display.cpp
@@ -0,0 +1,17 @@
+// lcd_display.cpp
+
+#include <LiquidCrystal.h>
+
+// Initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Hello, World!");
+}
+
+void loop() {
+  // Do nothing here...
+}
